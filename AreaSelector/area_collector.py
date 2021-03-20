57e30f5c0813388e4f1d50153924a0b71b8fe03d
Fr4nt1x <Fr4nt1x@Muc.de>
2021-03-20 13:44:08 +0100

    Added contour and numering

--- AreaSelector/area_collector.py
+++ AreaSelector/area_collector.py
@@ -3,16 +3,40 @@ import cv2 as cv
 
 
 class AreaCollector(object):
-    def __init__(self, input_image):
-        self._input_image = cv.imread(input_image)
+    def __init__(self, input_image, colors):
+        self._input_image = input_image
+        self._colors = colors
+
+    def show(self, image):
+        image_array = image
+        if len(image.shape) >= 3:
+            image_array = cv.cvtColor(image, cv.COLOR_RGB2BGR)
+        cv.imshow('area-collector', image_array)
+        cv.waitKey(0)
 
     def find_areas(self):
+        self.show(self._input_image)
+        index = 0
+        for color in self._colors:
+            print("Processing Color (R,G,B) : {}".format(color))
+            mask = np.array(np.all(self._input_image == np.asarray(color), axis=2), dtype=np.uint8)
+            img_one_color = cv.bitwise_and(self._input_image, self._input_image, mask=mask)
+            self.show(img_one_color)
+            image_gray = cv.cvtColor(img_one_color, cv.COLOR_RGB2GRAY)
+            contours, hierachy = cv.findContours(image_gray, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-        imgray = cv.cvtColor(self._input_image, cv.COLOR_BGR2GRAY)
-        ret, thresh = cv.threshold(imgray, 127, 255, 0)
-        contours, hierachy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        output = np.zeros(self._input_image.shape)
-        cv.drawContours(output, contours, -1, (0, 255, 0), 1)
+            output = 255*np.ones(self._input_image.shape, dtype=np.uint8)
+            cv.drawContours(output, contours, -1, (0, 0, 0), 1)
+            self.show(output)
 
-        cv.imshow('bla', output)
-        cv.waitKey(0)
+            for contour in contours:
+                rect = cv.minAreaRect(contour)
+                font = cv.FONT_HERSHEY_SIMPLEX
+                font_color = (0, 255, 0)
+                text = str(index)
+                font_size = 0.25
+                font_thickness = 1
+                output = cv.putText(output, text, (int(rect[0][0]), int(rect[0][1])), font, font_size, font_color,
+                                    font_thickness, cv.LINE_AA)
+                self.show(output)
+            index += 1
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageFilter
-
+import cv2 as cv
 import numpy as np
 from colorthief import ColorThief
 from ColorReducer.color_reducer import ColorReducer
@@ -12,15 +12,14 @@ if __name__ == "__main__":
 
     color_thief = ColorThief(path_to_image)
     # build a color palette
-    colors = np.asarray(color_thief.get_palette(color_count=12, quality=1), dtype=np.int64)
+    colors = np.unique(np.asarray(color_thief.get_palette(color_count=16, quality=1), dtype=np.int64), axis=0)
 
-    im = Image.open(path_to_image)
-    im = im.filter(ImageFilter.GaussianBlur(2))
-    #im.show()
-    image_array = np.array(im)
+    image_array = cv.imread(path_to_image)
+    image_array = cv.cvtColor(image_array, cv.COLOR_BGR2RGB)
+
     color_reducer = ColorReducer(image_array, colors)
     color_reducer.reduce_color()
-    area_collector = AreaCollector(path_to_image)
+    area_collector = AreaCollector(color_reducer.output_image, colors)
     area_image = area_collector.find_areas()
     new_image_array = np.array(color_reducer.output_image)
     Image.fromarray(color_reducer.output_image).save(path_output_image)
