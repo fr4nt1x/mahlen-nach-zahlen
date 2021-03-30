d8e1dbeb88ff21c3362bb14ae74e08fc91b56086
Fr4nt1x <Fr4nt1x@Muc.de>
2021-03-30 21:20:12 +0200

    Print color index at boundary area

--- AreaSelector/area_collector.py
+++ AreaSelector/area_collector.py
@@ -7,6 +7,7 @@ class AreaCollector(object):
         self._input_image = input_image
         self._colors = colors
         self._min_area = min_area
+        self._output_image = 255 * np.ones(self._input_image.shape, dtype=np.uint8)
 
     def show(self, image):
         image_array = image
@@ -15,28 +16,43 @@ class AreaCollector(object):
         cv.imshow('area-collector', image_array)
         cv.waitKey(0)
 
+    def preprocess_image(self):
+        output_image = cv.medianBlur(self._input_image, 11)
+        self.show(output_image)
+        self._input_image = output_image
+
     def find_areas(self):
-        self.show(self._input_image)
         index = 0
-        output = 255 * np.ones(self._input_image.shape, dtype=np.uint8)
+
         for color in self._colors:
             print("Processing Color (R,G,B) : {}".format(color))
             mask = np.array(np.all(self._input_image == np.asarray(color), axis=2), dtype=np.uint8)
             img_one_color = cv.bitwise_and(self._input_image, self._input_image, mask=mask)
             image_gray = cv.cvtColor(img_one_color, cv.COLOR_RGB2GRAY)
-            contours, hierachy = cv.findContours(image_gray, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+            contours, hierachy = cv.findContours(image_gray, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
             reduced_contours = [c for c in contours if abs(cv.contourArea(c, False)) > self._min_area]
 
-            cv.drawContours(output, reduced_contours, -1, (0, 0, 0), 1)
+            cv.drawContours(self._output_image, reduced_contours, -1, (0, 0, 0), 1)
 
             for contour in reduced_contours:
-                rect = cv.minAreaRect(contour)
-                font = cv.FONT_HERSHEY_SIMPLEX
-                font_color = (0, 255, 0)
-                text = str(index)
-                font_size = 0.25
-                font_thickness = 1
-                output = cv.putText(output, text, (int(rect[0][0]), int(rect[0][1])), font, font_size, font_color,
-                                    font_thickness, cv.LINE_AA)
+                self._put_color_at_contour(contour, index)
+
             index += 1
-        self.show(output)
+        self.show(self._output_image)
+
+    def _put_color_at_contour(self, contour, index):
+        font = cv.FONT_HERSHEY_SIMPLEX
+        font_color = (100, 100, 100)
+        text = str(index)
+        font_size = 0.4
+        font_thickness = 1
+        vec = contour[0, 0, :] - contour[1, 0, :]
+        mat = np.array([[0, -1], [1, 0]])
+        vec_orth = np.dot(vec, mat)/np.linalg.norm(vec)
+        start_point = contour[0, 0, :]
+        end_point = contour[0, 0, :] - vec_orth*20
+        end_point = end_point.astype(np.int64)
+        self._output_image = cv.line(self._output_image, tuple(start_point), tuple(end_point), font_color,
+                                     font_thickness)
+        self._output_image = cv.putText(self._output_image, text, (int(end_point[0]), int(end_point[1])), font,
+                                        font_size, font_color, font_thickness, cv.LINE_AA)
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from PIL import Image, ImageFilter
 import cv2 as cv
 import numpy as np
 from colorthief import ColorThief
@@ -7,20 +6,21 @@ from AreaSelector.area_collector import AreaCollector
 
 
 if __name__ == "__main__":
-    path_to_image = 'data/test.jpg'
+    path_to_image = 'data/Elefant.jpg'
     path_output_image = 'data/reducedElefantOut.bmp'
-
-    color_thief = ColorThief(path_to_image)
-    # build a color palette
-    colors = np.unique(np.asarray(color_thief.get_palette(color_count=16, quality=1), dtype=np.int64), axis=0)
 
     image_array = cv.imread(path_to_image)
     image_array = cv.cvtColor(image_array, cv.COLOR_BGR2RGB)
+    color_thief = ColorThief(path_to_image)
+
+    # build a color palette
+    colors = np.unique(np.asarray(color_thief.get_palette(color_count=8, quality=1), dtype=np.int64), axis=0)
 
     color_reducer = ColorReducer(image_array, colors)
     color_reducer.reduce_color()
 
-    area_collector = AreaCollector(color_reducer.output_image, colors, 2000)
+    area_collector = AreaCollector(color_reducer.output_image, colors, 500)
+    area_collector.preprocess_image()
     area_image = area_collector.find_areas()
-    new_image_array = np.array(color_reducer.output_image)
+    #new_image_array = np.array(color_reducer.output_image)
     #Image.fromarray(color_reducer.output_image).filter(ImageFilter.SHARPEN).save(path_output_image)
